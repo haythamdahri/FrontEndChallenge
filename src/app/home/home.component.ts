c6a1bd1b415f6b96598cc96958048e063d7d4c55
haythamdahri <haytham.dahri@gmail.com>
2019-04-14 12:04:43 +0100

    Fixex

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,13 +21,16 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.route.queryParams.subscribe(
       (params: Params) => {
-        if (params['id'] != null) {
-          this.currentPage = params['id'];
+        if (params['page'] != null) {
+          this.currentPage = +params['page'];
         } else {
           this.currentPage = 1;
         }
         console.log('page: ' + this.currentPage);
         console.log('Page type: ' + typeof this.currentPage);
+        if (this.repositories != null || this.repositories.length > 0) {
+          this.repositories = [];
+        }
         const tempRepositories: Repository[] = this.githubService.getRepositories(this.currentPage);
         if (tempRepositories != null) {
           this.isError = false;
