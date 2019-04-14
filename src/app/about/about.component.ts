8a0d418c3780cc2e96d5056ae1da04a47f55ade8
haythamdahri <haytham.dahri@gmail.com>
2019-04-14 11:48:36 +0100

    About Page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  imagePath: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.imagePath = './card.jpeg';
   }
 
 }
